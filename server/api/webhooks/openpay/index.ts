b78998faa20c1e36a650edffb20ceb8d3a6a4e00
Sr-3pp <martin.ru@outlook.com>
2024-08-19 11:44:31 -0600

    add webhook route

--- server/api/webhooks/openpay/index.ts
+++ server/api/webhooks/openpay/index.ts
@@ -1,8 +1,6 @@
 const { public: config } = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
-  const webhooks = await $fetch(
-    `${config.openpay.url}/${config.openpay.merchantId}/webhooks`,
-  );
-  return webhooks;
+  console.log("event", event);
+  return true;
 });
