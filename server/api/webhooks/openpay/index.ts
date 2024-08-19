e017ff96d7ec9454c49ad925a79b5cf2bfde2cc7
Sr-3pp <martin.ru@outlook.com>
2024-08-19 12:45:16 -0600

    get verification code

--- server/api/webhooks/openpay/index.ts
+++ server/api/webhooks/openpay/index.ts
@@ -3,13 +3,14 @@ const { public: config } = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
   //if is post request
   if (event.method === "POST") {
+    console.log("start post: -------------");
     const data = await readBody(event);
-    console.log("the webhook index: -------------", data);
+    console.log("the webhook post: -------------", data);
     return {
       message: "webhook received as post",
     };
   }
-  console.log("the webhook index: -------------");
+  console.log("the webhook get: -------------");
   return {
     message: "webhook received as get",
   };
